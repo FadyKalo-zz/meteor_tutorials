cf468f92a1e39473c7a61e39a400eb8feec0fae7
fadykalo <fadykalo@gmail.com>
2014-05-17 18:15:10 +0100

    Sort posts by timestamp.

--- microscope/collections/posts.js
+++ microscope/collections/posts.js
@@ -20,12 +20,11 @@ Meteor.methods({
             throw new Meteor.Error(422, 'Please fill in a headline');
         // check that there are no previous posts with the same link
         if (postAttributes.url && postWithSameLink) {
-            throw new Meteor.Error(302,
-                'This link has already been posted',
-                postWithSameLink._id);
+            throw new Meteor.Error(302, 'This link has already been posted', postWithSameLink._id);
         }
         // pick out the whitelisted keys
-        var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), { userId: user._id,
+        var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+            userId: user._id,
             author: user.username,
             submitted: new Date().getTime()
         });
