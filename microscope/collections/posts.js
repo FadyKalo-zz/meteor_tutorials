69f53181cdc098ce7ef07590abd304d34bd51ad6
fadykalo <fadykalo@gmail.com>
2014-05-17 19:26:21 +0100

    Only allow changing certain fields of posts.

--- microscope/collections/posts.js
+++ microscope/collections/posts.js
@@ -8,8 +8,15 @@ Posts = new Meteor.Collection('posts');
 //});
 
 Posts.allow({
-  update: ownsDocument,
-  remove: ownsDocument
+    update: ownsDocument,
+    remove: ownsDocument
+});
+
+Posts.deny({
+    update: function (userId, post, fieldNames) {
+        // may only edit the following two fields:
+        return (_.without(fieldNames, 'url', 'title').length > 0);
+    }
 });
 
 Meteor.methods({
